adc4710399f3069cd11a11f24cc2c7ee44e8bffc
Victorgichohi <victorgichohi48@gmail.com>
2016-02-21 11:46:48 +0300

    creaed posts model and added it to the admin

---
+++ envs/trydjango2env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/victor/Desktop/Django/Django_Apps/envs/trydjango2env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
