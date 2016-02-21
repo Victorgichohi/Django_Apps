200d4956335fb2655454d8b029b1f5611a454347
Victorgichohi <victorgichohi48@gmail.com>
2016-02-21 13:21:41 +0300

    Added some wuery set and its working dynamically on the views

--- trydjango1.9/trydjango19/posts/views.py
+++ trydjango1.9/trydjango19/posts/views.py
@@ -2,15 +2,26 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def post_create(request):
 	return HttpResponse("<h1>create</h1>")
 
 def post_detail(request):
-	return HttpResponse("<h1>detail</h1>")
+	# return HttpResponse("<h1>detail</h1>")
+
+	
+	return render(request, "index.html" ,{})
 
 def post_list(request):
-	return HttpResponse("<h1>list</h1>")
+	queryset=Post.objects.all()
+	context={
+	"object_list":queryset
+	}
+
+	return render(request,"index.html",context)
+	# return HttpResponse("<h1>list</h1>")
 
 def post_update(request):
 	return HttpResponse("<h1>update</h1>")
